bd9251293de5585bbf3920f82cebaf7fdc6b00d3
Leandro Sales <leandroasp@gmail.com>
2021-08-30 21:21:39 -0300

    Update para categoria implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { HomeComponent } from "./components/views/home/home.component";
 import { CategoriaReadComponent } from "./components/views/categoria/categoria-read/categoria-read.component";
 import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
 import { CategoriaDeleteComponent } from './components/views/categoria/categoria-delete/categoria-delete.component';
+import { CategoriaUpdateComponent } from './components/views/categoria/categoria-update/categoria-update.component';
 
 @NgModule({
   declarations: [
@@ -35,6 +36,7 @@ import { CategoriaDeleteComponent } from './components/views/categoria/categoria
     CategoriaReadComponent,
     CategoriaCreateComponent,
     CategoriaDeleteComponent,
+    CategoriaUpdateComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/views/categoria/categoria-update/categoria-update.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Categoria } from "../categoria.model";
+import { CategoriaService } from "../categoria.service";
+
+@Component({
+  selector: "app-categoria-update",
+  templateUrl: "./categoria-update.component.html",
+  styleUrls: ["./categoria-update.component.css"],
+})
+export class CategoriaUpdateComponent implements OnInit {
+  categoria: Categoria = {
+    nome: "",
+    descricao: "",
+  };
+
+  constructor(
+    private service: CategoriaService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.categoria.id = this.route.snapshot.paramMap.get("id")!;
+    this.findById();
+  }
+
+  findById(): void {
+    this.service.findById(this.categoria.id!).subscribe((resposta) => {
+      this.categoria.nome = resposta.nome;
+      this.categoria.descricao = resposta.descricao;
+    });
+  }
+
+  update(): void {
+    this.service.update(this.categoria).subscribe(
+      (resposta) => {
+        this.router.navigate([`categorias/update/${this.categoria.id}`]);
+        this.service.mensagem("Categoria atualizada com sucesso");
+      },
+      (err) => {
+        if (err.error.errors.length > 0) {
+          this.service.mensagem(err.error.errors[0].message);
+        }
+      }
+    );
+  }
+
+  cancel(): void {
+    this.router.navigate(["categorias"]);
+  }
+}
